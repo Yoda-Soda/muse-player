a9643be1e86f0e5a5e575386e4a5ffa24c6be126
Jan Claasen <janc@cloudcannon.com>
2022-02-05 20:50:05 +1300

    Feat: connect front end to api

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,40 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { onMount } from 'svelte';
+	let topSongName;
+	let title;
+
+	onMount(async () => {
+		const response = await fetch('http://127.0.0.1:5002/music/Three');
+		// const response = await fetch('https://api.publicapis.org/entries');
+		const songs = await response.json();
+		// topSongName = songs;
+		topSongName = songs[0];
+		console.log(typeof title, songs);
+	});
+</script>
+
+<section>
+	<h1>Muse Player</h1>
+	<h2>TopSong</h2>
+	<input type="search" name="" id="" placeholder="Search Song" />
+	<code>
+		{#if topSongName === undefined}
+			Loading Song Name...
+		{:else}
+			{topSongName.title}
+		{/if}
+	</code>
+</section>
+
+<style>
+	section {
+		display: grid;
+		align-content: center;
+		max-width: 500px;
+	}
+	code {
+		background-color: black;
+		color: blanchedalmond;
+		padding: 3rem;
+	}
+</style>
---
+++ src/routes/test.svelte
@@ -0,0 +1,20 @@
+<script>
+	import { onMount } from 'svelte';
+	let character;
+
+	onMount(async () => {
+		const response = await fetch('https://swapi.dev/api/people/1');
+		const data = await response.json();
+		character = data;
+	});
+</script>
+
+<main>
+	{#if character === undefined}
+		Loading character...
+	{:else}
+		{character.name}
+		<br />
+		{character.mass}
+	{/if}
+</main>
