d5a708ed7f71854dfe2babd809a883c7985b6d12
Jan Claasen <janc@cloudcannon.com>
2022-02-05 23:18:36 +1300

    Feat: Display multipal songs

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,15 +1,25 @@
 <script>
 	import { onMount } from 'svelte';
-	let topSongName;
-	let title;
+	let songs;
+	let songImage;
+
+	const getSongs = (songListData) => {
+		const songList = songListData.filter((song) => song.resultType === 'song');
+		return songList;
+	};
+
+	const getMaxImageSizeURL = (song) => {
+		let imageLenght = song.thumbnails.slice(-1);
+		return imageLenght[0].url;
+	};
 
 	onMount(async () => {
 		const response = await fetch('http://127.0.0.1:5002/music/Three');
 		// const response = await fetch('https://api.publicapis.org/entries');
-		const songs = await response.json();
+		const data = await response.json();
 		// topSongName = songs;
-		topSongName = songs[0];
-		console.log(typeof title, songs);
+		songs = getSongs(data);
+		console.log(songs);
 	});
 </script>
 
@@ -17,13 +27,14 @@
 	<h1>Muse Player</h1>
 	<h2>TopSong</h2>
 	<input type="search" name="" id="" placeholder="Search Song" />
-	<code>
-		{#if topSongName === undefined}
-			Loading Song Name...
-		{:else}
-			{topSongName.title}
-		{/if}
-	</code>
+	{#if songs == undefined}
+		Loading songs
+	{:else}
+		{#each songs as song}
+			<h3>{song.title}</h3>
+			<img src={getMaxImageSizeURL(song)} alt="" />
+		{/each}
+	{/if}
 </section>
 
 <style>
@@ -32,9 +43,21 @@
 		align-content: center;
 		max-width: 500px;
 	}
-	code {
+	h1 {
+		font-size: 32px;
+	}
+	h2 {
+		font-size: 28px;
+	}
+	h3 {
+		font-size: 21px;
+	}
+	.song {
 		background-color: black;
 		color: blanchedalmond;
 		padding: 3rem;
+	}
+	.song img {
+		width: 100%;
 	}
 </style>
