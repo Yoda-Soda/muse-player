5cb9ea2302731bc33a0f9f40b1441d4bf3b1b99c
Jan Claasen <janc@cloudcannon.com>
2022-02-18 00:04:59 +1300

    Chore: update api server address

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,7 @@
 	let songs;
 	let timer;
 	let statusMsg = 'Try searching for songs';
-	const apiServer = 'http://192.168.1.11:4444';
+	const apiServer = 'http://test.janclaasen.me:4444';
 	let isTopOfPage = false;
 
 	const setTopOfPage = () => {
